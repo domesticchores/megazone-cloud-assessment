029d7fc71105c0987ddee85224662a11f9a576f1
Akash Keshav <112591754+domesticchores@users.noreply.github.com>
2025-11-22 16:42:28 -0500

    create  basic layout of site

--- weather-app/src/Grid.tsx
+++ weather-app/src/Grid.tsx
@@ -1,9 +1,23 @@
 
 function Grid() {
   return (
-    <div className="grid w-screen h-screen md:grid-cols-3 sm:grid-cols-1 pt-12">
-        <div className="bg-yellow-100">Left</div>
-        <div className="bg-yellow-100 md:col-span-2">Right</div>
+    <div className="bg-yellow-200 w-screen h-screen">
+        <div className="grid w-screen h-screen md:grid-cols-5 sm:grid-cols-1 pt-12 max-w-6xl mx-auto">
+            <div className="md:col-span-2 p-8">
+                <div className="bg-yellow-500 h-full rounded-4xl p-8">Box1</div>
+            </div>
+            <div className="md:col-span-3 grid p-8 gap-x-8 gap-y-4">
+                <div className="bg-yellow-500 h-full rounded-4xl p-8">Box1</div>
+
+                <div className="grid grid-cols-3 gap-x-4 h-full">
+                    <div className="bg-yellow-500 h-full rounded-4xl p-8">Box1</div>
+                    <div className="bg-yellow-500 h-full rounded-4xl p-8">Box1</div>
+                    <div className="bg-yellow-500 h-full rounded-4xl p-8">Box1</div>
+                </div>
+
+                <div className="bg-yellow-500 h-full rounded-4xl p-8">Box1</div>
+            </div>
+        </div>
     </div>
   )
 }
--- weather-app/src/NavBar.tsx
+++ weather-app/src/NavBar.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Logo from './assets/react.svg'
 
 function NavBar() {
   return (
     <>
-      <nav className='fixed flex h-12 bg-amber-300 z-10 w-screen'>
-        <p>Home</p>
+      <nav className='fixed flex h-12 bg-amber-300 z-10 w-screen items-center px-8'>
+        <div className='h-fit flex gap-x-2'>
+            <img src={Logo} alt="Logo" />
+            <h1 className='text-3xl font-bold'>Duumori</h1>
+        </div>
       </nav>
     </>
   )
