3f7160a18e76bd27c14e3db6943dc975a290c24b
Akash Keshav <112591754+domesticchores@users.noreply.github.com>
2025-11-22 23:58:57 -0500

    initial styling complete

--- weather-app/src/Grid.tsx
+++ weather-app/src/Grid.tsx
@@ -3,24 +3,152 @@ function Grid() {
     // main container
     <div className="bg-yellow-200 w-screen h-screen pt-12">
         {/* grid container, with horizontal padding */}
-        <div className="grid w-screen h-full grid-rows-3 md:grid-cols-6 sm:grid-cols-1 gap-4 max-w-7xl mx-auto py-8">
-            <div className="bg-yellow-500 h-full rounded-4xl p-8 col-span-1 row-span-3">Box1</div>
-            <div className="bg-yellow-500 h-full rounded-4xl p-8 col-span-2 row-span-3">
-                <div className="w-full h-1/2 bg-amber-600 flex">
-                    <span className="material-symbols-rounded text-[10em]! md:text-[12em]! lg:text-[15em]! m-auto">moon_stars</span>
-                </div>
-                <div className="w-full h-1/5 bg-amber-700">
-                    
-                </div>
-                <div className="w-full h-3/10 bg-amber-800">
+        <div className="grid w-screen h-full grid-rows-3 md:grid-cols-6 sm:grid-cols-1 gap-4 max-w-7xl mx-auto py-8 px-4">
+            <div className="bg-yellow-500 h-full rounded-4xl col-span-1 row-span-2 md:row-span-3 overflow-hidden">
+                <div className="w-full h-full p-8 overflow-y-scroll scrollbar-thin scrollbar-thumb-amber-800 scrollbar-track-amber-600">
+                <h1 className="text-3xl font-semibold text-center mb-4">Favorites</h1>
+                <div className="grid gap-8 h-full">
+                    <div className="m-auto text-center">
+                        <h2 className="text-2xl font-bold">London</h2>
+                        <span className="material-symbols-rounded text-[6.5em]! md:text-[12em]! lg:text-[4em]! m-auto">air</span>
+                        <div className="flex w-fit text-2xl font-bold gap-x-1 items-center m-auto -mt-2">
+                            <h2 className="">43°</h2>
+                            <h2 className="">/</h2>
+                            <h2 className="">29°</h2>
+                        </div>
+                    </div>
 
+                    <div className="m-auto text-center">
+                        <h2 className="text-2xl font-bold">Paris</h2>
+                        <span className="material-symbols-rounded text-[6.5em]! md:text-[12em]! lg:text-[4em]! m-auto">air</span>
+                        <div className="flex w-fit text-2xl font-bold gap-x-1 items-center m-auto -mt-2">
+                            <h2 className="">43°</h2>
+                            <h2 className="">/</h2>
+                            <h2 className="">29°</h2>
+                        </div>
+                    </div>
+
+                    <div className="m-auto text-center">
+                        <h2 className="text-2xl font-bold">Paris</h2>
+                        <span className="material-symbols-rounded text-[6.5em]! md:text-[12em]! lg:text-[4em]! m-auto">air</span>
+                        <div className="flex w-fit text-2xl font-bold gap-x-1 items-center m-auto -mt-2">
+                            <h2 className="">43°</h2>
+                            <h2 className="">/</h2>
+                            <h2 className="">29°</h2>
+                        </div>
+                    </div>
+
+                    <div className="m-auto text-center">
+                        <h2 className="text-2xl font-bold">Paris</h2>
+                        <span className="material-symbols-rounded text-[6.5em]! md:text-[12em]! lg:text-[4em]! m-auto">air</span>
+                        <div className="flex w-fit text-2xl font-bold gap-x-1 items-center m-auto -mt-2">
+                            <h2 className="">43°</h2>
+                            <h2 className="">/</h2>
+                            <h2 className="">29°</h2>
+                        </div>
+                    </div>
+                </div>
                 </div>
             </div>
-            <div className="bg-yellow-500 h-full rounded-4xl p-8 col-span-1">Box1</div>
-            <div className="bg-yellow-500 h-full rounded-4xl p-8 col-span-1">Box1</div>
-            <div className="bg-yellow-500 h-full rounded-4xl p-8 col-span-1">Box1</div>
-            <div className="bg-yellow-500 h-full rounded-4xl p-8 col-span-3">Box1</div>
-            <div className="bg-yellow-500 h-full rounded-4xl p-8 col-span-3">Box1</div>
+            <div className="bg-yellow-500 h-full rounded-4xl p-8 col-span-2 row-span-2 md:row-span-3">
+                <div className="w-full h-1/2 flex">
+                    <span className="material-symbols-rounded text-[6.5em]! md:text-[12em]! lg:text-[15em]! m-auto">moon_stars</span>
+                </div>
+                <div className="w-full h-2/12 text-center">
+                    <h1 className="text-3xl">Partly Cloudy.</h1>
+                </div>
+                <div className="w-full h-4/12 text-center">
+                    <h1 className="text-8xl font-bold">35°C</h1>
+                    <p className="text-2xl">Actual: 40°</p>
+                </div>
+            </div>
+            <div className="bg-yellow-500 h-full rounded-4xl p-2 col-span-1 flex">
+                <div className="m-auto text-center">
+                    <span className="material-symbols-rounded text-[6.5em]! md:text-[12em]! lg:text-[7em]! m-auto">air</span>
+                    <h2 className="text-xl font-bold">7.6 MPH</h2>
+                </div>
+            </div>
+            <div className="bg-yellow-500 h-full rounded-4xl p-2 col-span-1 flex">
+                <div className="m-auto text-center">
+                    <span className="material-symbols-rounded text-[6.5em]! md:text-[12em]! lg:text-[7em]! m-auto">water_voc</span>
+                    <h2 className="text-xl font-bold">53% Humidity</h2>
+                </div>
+            </div>
+            <div className="bg-yellow-500 h-full rounded-4xl p-2 col-span-1 flex">
+                <div className="m-auto text-center">
+                    <span className="material-symbols-rounded text-[6.5em]! md:text-[12em]! lg:text-[7em]! m-auto">sunny</span>
+                    <h2 className="text-xl font-bold">UV Index 0</h2>
+                </div>
+            </div>
+            <div className="bg-yellow-500 h-full rounded-4xl p-2 col-span-3 flex">
+                <div className="m-auto text-center">
+                    <h2 className="text-2xl font-bold">NOW</h2>
+                    <span className="material-symbols-rounded text-[6.5em]! md:text-[12em]! lg:text-[4em]! m-auto">air</span>
+                    <div className="flex w-fit text-2xl font-bold gap-x-1 items-center m-auto -mt-2">
+                        <h2 className="">43°</h2>
+                        <h2 className="">/</h2>
+                        <h2 className="">29°</h2>
+                    </div>
+                </div>
+
+                <div className="m-auto text-center">
+                    <h2 className="text-2xl font-bold">11AM</h2>
+                    <span className="material-symbols-rounded text-[6.5em]! md:text-[12em]! lg:text-[4em]! m-auto">air</span>
+                    <div className="flex w-fit text-2xl font-bold gap-x-1 items-center m-auto -mt-2">
+                        <h2 className="">43°</h2>
+                        <h2 className="">/</h2>
+                        <h2 className="">29°</h2>
+                    </div>
+                </div>
+
+                <div className="m-auto text-center">
+                    <h2 className="text-2xl font-bold">12PM</h2>
+                    <span className="material-symbols-rounded text-[6.5em]! md:text-[12em]! lg:text-[4em]! m-auto">air</span>
+                    <div className="flex w-fit text-2xl font-bold gap-x-1 items-center m-auto -mt-2">
+                        <h2 className="">43°</h2>
+                        <h2 className="">/</h2>
+                        <h2 className="">29°</h2>
+                    </div>
+                </div>
+
+                <div className="m-auto text-center">
+                    <h2 className="text-2xl font-bold">1PM</h2>
+                    <span className="material-symbols-rounded text-[6.5em]! md:text-[12em]! lg:text-[4em]! m-auto">air</span>
+                    <div className="flex w-fit text-2xl font-bold gap-x-1 items-center m-auto -mt-2">
+                        <h2 className="">43°</h2>
+                        <h2 className="">/</h2>
+                        <h2 className="">29°</h2>
+                    </div>
+                </div>
+
+                <div className="m-auto text-center">
+                    <h2 className="text-2xl font-bold">2PM</h2>
+                    <span className="material-symbols-rounded text-[6.5em]! md:text-[12em]! lg:text-[4em]! m-auto">air</span>
+                    <div className="flex w-fit text-2xl font-bold gap-x-1 items-center m-auto -mt-2">
+                        <h2 className="">43°</h2>
+                        <h2 className="">/</h2>
+                        <h2 className="">29°</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-yellow-500 h-full rounded-4xl py-4 px-12 col-span-3 flex">
+                <div className="w-1/2 text-center flex">
+                    <div className="w-fit m-auto">
+                        <h2 className="text-2xl font-semibold">Tomorrow's Forecast</h2>
+                        <p className="my-auto text-xl text-center w-full">Patchy rain nearby.</p>
+                    </div>
+                </div>
+                <div className="w-1/2">
+                    <div className="w-fit m-auto">
+                        <span className="material-symbols-rounded text-[6.5em]! md:text-[12em]! lg:text-[7em]! flex m-auto">rainy</span>
+                    </div>
+                    <div className="flex w-fit text-2xl font-bold gap-x-1 items-center m-auto -mt-2">
+                        <h2 className="">43°</h2>
+                        <h2 className="text-3xl">/</h2>
+                        <h2 className="">29°</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
--- weather-app/src/NavBar.tsx
+++ weather-app/src/NavBar.tsx
@@ -4,7 +4,7 @@ import Logo from './assets/react.svg'
 function NavBar() {
   return (
     <>
-      <nav className='fixed flex h-12 bg-amber-300 z-10 w-screen items-center px-8'>
+      <nav className='fixed flex h-16 bg-amber-300 z-10 w-screen items-center px-8'>
         <div className='h-fit flex gap-x-2'>
             <img src={Logo} alt="Logo" />
             <h1 className='text-3xl font-bold'>Duumori</h1>
