7a525645af4b09015b16f9fdbaca565af6ba51f7
Akash Keshav <112591754+domesticchores@users.noreply.github.com>
2025-11-22 21:47:34 -0500

    rework grid to be easier to manipulate

--- weather-app/src/Grid.tsx
+++ weather-app/src/Grid.tsx
@@ -1,22 +1,26 @@
-
 function Grid() {
   return (
-    <div className="bg-yellow-200 w-screen h-screen">
-        <div className="grid w-screen h-screen md:grid-cols-5 sm:grid-cols-1 pt-12 max-w-6xl mx-auto">
-            <div className="md:col-span-2 p-8">
-                <div className="bg-yellow-500 h-full rounded-4xl p-8">Box1</div>
-            </div>
-            <div className="md:col-span-3 grid p-8 gap-x-8 gap-y-4">
-                <div className="bg-yellow-500 h-full rounded-4xl p-8">Box1</div>
-
-                <div className="grid grid-cols-3 gap-x-4 h-full">
-                    <div className="bg-yellow-500 h-full rounded-4xl p-8">Box1</div>
-                    <div className="bg-yellow-500 h-full rounded-4xl p-8">Box1</div>
-                    <div className="bg-yellow-500 h-full rounded-4xl p-8">Box1</div>
+    // main container
+    <div className="bg-yellow-200 w-screen h-screen pt-12">
+        {/* grid container, with horizontal padding */}
+        <div className="grid w-screen h-full grid-rows-3 md:grid-cols-6 sm:grid-cols-1 gap-4 max-w-7xl mx-auto py-8">
+            <div className="bg-yellow-500 h-full rounded-4xl p-8 col-span-1 row-span-3">Box1</div>
+            <div className="bg-yellow-500 h-full rounded-4xl p-8 col-span-2 row-span-3">
+                <div className="w-full h-1/2 bg-amber-600 flex">
+                    <span className="material-symbols-rounded text-[10em]! md:text-[12em]! lg:text-[15em]! m-auto">moon_stars</span>
                 </div>
+                <div className="w-full h-1/5 bg-amber-700">
+                    
+                </div>
+                <div className="w-full h-3/10 bg-amber-800">
 
-                <div className="bg-yellow-500 h-full rounded-4xl p-8">Box1</div>
+                </div>
             </div>
+            <div className="bg-yellow-500 h-full rounded-4xl p-8 col-span-1">Box1</div>
+            <div className="bg-yellow-500 h-full rounded-4xl p-8 col-span-1">Box1</div>
+            <div className="bg-yellow-500 h-full rounded-4xl p-8 col-span-1">Box1</div>
+            <div className="bg-yellow-500 h-full rounded-4xl p-8 col-span-3">Box1</div>
+            <div className="bg-yellow-500 h-full rounded-4xl p-8 col-span-3">Box1</div>
         </div>
     </div>
   )
