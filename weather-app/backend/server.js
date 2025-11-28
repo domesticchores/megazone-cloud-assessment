1b24858d4b75cb6a50e0c7a25af687748119b314
Akash Keshav <112591754+domesticchores@users.noreply.github.com>
2025-11-27 22:12:15 -0600

    implement main request

--- weather-app/backend/server.js
+++ weather-app/backend/server.js
@@ -1,11 +1,41 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
-const port = 3000
+
+const headers = {
+  'Content-Type': 'application/json',
+  'key': process.env.WEATHER_API_KEY,
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.use(express.json())
+
+app.post('/main', async (req, res) => {
+  try {
+  const city = req.body.city;
+  if (!city) {
+    return res.status(400).json({message: "Bad City Request!"});
+  }
+  const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?q=${city}&days=2`,{
+      method: 'GET',
+      headers: headers
+  });
+  if (!response.ok) {
+    const err = await response.json();
+      console.error("could not get response: ", err);
+      return res.status(400).json();
+  }
+
+  res.json(await response.json());
+  } catch (err) {
+    console.log("server error: ", err)
+    return res.status(500).json({message: "Internal Server Error", error: err})
+  }
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`)
 })
